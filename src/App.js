44dc91962b6e042aa85970faf8c3e81cd76fef3c
MargaretTechworld <margarettechworld@gmail.com>
2023-08-16 14:54:46 +0000

    add routes to home, calculator and qoutes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import {
+  createBrowserRouter, createRoutesFromElements, Route, RouterProvider,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 import Quote from './components/Quote';
 import './App.css';
+import RootLayout from './routes/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="calculator" element={<Calculator />} />
+      <Route path="quote" element={<Quote />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
-    <div className="main-container">
-      {' '}
-      <Quote />
-      {' '}
-      <Calculator />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
